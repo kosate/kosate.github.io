93ea77553c674f088426261b0f879ab53f36e1a3
kosate <kosate@gmail.com>
2024-02-07 22:01:16 +0900

    addon comments

---
+++ assets/js/getComments.js
@@ -0,0 +1,44 @@
+// 함수를 정의하여 인자로 받은 값으로 처리
+$(document).ready(function() {
+
+    var repository = "kosate/comments";
+    $.ajax({
+    url: "https://api.github.com/repos/" + repository+"/issues",
+    type: 'GET', 
+    dataType: "json",
+    success: function(data) {
+
+        var issueDataMap = new Map();
+        // 조회 건수가 있으면
+        if (data.length > 0) {
+            // 각 이슈의 제목과 댓글 수 출력
+            for (var i = 0; i < data.length; i++) {
+                var issueTitle = data[i].title;
+                var commentsCount = data[i].comments;
+
+                // Map에 저장
+                issueDataMap.set("/"+issueTitle, commentsCount);
+            }
+         
+            // archive__item 클래스 내의 a 태그 정보 가져오기
+            var archiveItemATags = $('.archive__item a');
+            var archiveCommentsTags = $('.archive__item .comment_txt');
+
+            for (var i = 0; i < archiveItemATags.length; i++) {
+                var key = archiveItemATags.eq(i).attr('href');
+                var value = issueDataMap.get(key); 
+                // value가 undefined인 경우 0으로 대체
+                if (value === undefined) {
+                    value = 0;
+                }
+                // 해당 이슈의 댓글 수를 표시
+                archiveCommentsTags.eq(i).text(value);
+            }
+        }
+    },
+    error: function(error) {
+        console.error('이슈 검색 실패:', error);
+    }
+    });
+
+});
